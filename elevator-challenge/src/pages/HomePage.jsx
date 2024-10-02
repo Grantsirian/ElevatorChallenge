e427b534ec2f701a7e83e3deb753dbba9f0d6e07
Grant Henderson <grantsirian@gmail.com>
2024-10-02 08:06:12 +0200

    Restructured components to allow for stylesheets

--- elevator-challenge/src/pages/HomePage.jsx
+++ elevator-challenge/src/pages/HomePage.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import Hero from '../components/Hero'
+import Hero from '../components/Hero/Hero'
 
 const HomePage = () => {
     const homeTitle = 'Elevator Products';
     const homeDescription = 'Elevator Products Description';
-    
+
     return (
         <>
             <Hero title={homeTitle} description={homeDescription} />
--- elevator-challenge/src/pages/ProductListPage.jsx
+++ elevator-challenge/src/pages/ProductListPage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Hero from '../components/Hero';
+import Hero from '../components/Hero/Hero';
 
 const ProductListPage = () => {
     const productListTitle = 'Products List';
